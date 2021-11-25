1063a2b289bd137ebe6ed424cec872e435e03e8e
David <ber.dev@yandex.ru>
2021-11-25 22:33:41 +0300

    Initial Commit

---
+++ RE_CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RE_CoreData
+//
+//  Created by Дэвид Бердников on 25.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
