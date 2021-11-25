092f8e355a6b98bfb73bfe181745d467500ed9fd
David <ber.dev@yandex.ru>
2021-11-25 23:41:32 +0300

    NavigationBar work

---
+++ RE_CoreData/TaskListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  RE_CoreData
+//
+//  Created by Дэвид Бердников on 25.11.2021.
+//
+
+import UIKit
+
+class TaskListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
